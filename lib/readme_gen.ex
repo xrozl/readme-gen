33349cb70a121121a9e1ed22c4a2b642f8e4b4f7
yuu <92621736+xrozl@users.noreply.github.com>
2022-05-28 09:31:01 +0900

    add base

--- lib/readme_gen.ex
+++ lib/readme_gen.ex
@@ -1,3 +1,13 @@
 defmodule ReadmeGen do
-  IO.puts "test"
+
+
+  fileName = IO.gets "Enter the name of the file to write result: "
+  fileName = Regex.replace(~r/\n/, fileName, "")
+  IO.puts "File Name: " <> fileName
+  result = ""
+  cache = IO.gets "Project Name: "
+  cache = Regex.replace(~r/\n/, cache, "")
+  result = "<h1 align=\"center\">" <> cache <> "</h1>"
+
+  IO.puts result
 end
